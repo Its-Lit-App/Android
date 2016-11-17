dfeebcecd27a8ef1c2e084e759ba04287617aba8
kevinkrames <kevinkrames@gmail.com>
2016-11-17 12:07:49 -0500

    Made it zoom to gainesville on startup by default

--- app/src/main/java/com/kac/its_lit_android/MapsActivity.java
+++ app/src/main/java/com/kac/its_lit_android/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.kac.its_lit_android;
 
 import android.app.Activity;
+import android.graphics.Camera;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -19,7 +20,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.Date;
@@ -30,13 +33,15 @@ import static android.widget.Toast.LENGTH_LONG;
 
 //GoogleMap.OnInfoWindowClickListener,
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnInfoWindowClickListener,
-        OnMapReadyCallback  {
+        GoogleMap.OnCameraMoveListener,
+        OnMapReadyCallback {
 
     private String title, content;
     private LatLng p;
     private GoogleMap mMap;
     private HashMap<Marker, eventInfo> eventMap;
     private DatabaseManager databaseManager;
+    private LatLng GAINESVILLE = new LatLng(29.6516, -82.3248);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +73,29 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnInfoWi
         });
 
         mMap.setOnInfoWindowClickListener(this);
+
+        //Setup listener for map moving
+
+        mMap.setOnCameraMoveListener(this);
+
+        //Default Map Location
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(GAINESVILLE));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        /*CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(GAINESVILLE)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));*/
+    }
+
+    //When the map is moved, this function is called:
+    @Override
+    public void onCameraMove() {
+
+
 
     }
 
