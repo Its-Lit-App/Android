7aa12f524a60d2ed3eb12338502395a1bb9ca219
dezayasa <dezayasa@ufl.edu>
2016-11-25 13:53:04 -0500

    GPS
    Added permission request to access GPS

--- app/src/main/java/com/kac/its_lit_android/MapsActivity.java
+++ app/src/main/java/com/kac/its_lit_android/MapsActivity.java
@@ -1,13 +1,20 @@
 package com.kac.its_lit_android;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 
 
@@ -22,8 +29,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import java.util.Date;
 import java.util.HashMap;
+import android.content.Context;
 import com.parse.*;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -38,6 +48,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnInfoWi
     private HashMap<Marker, eventInfo> eventMap;
     private DatabaseManager databaseManager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,16 +56,54 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnInfoWi
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         databaseManager = new DatabaseManager(this);
     }
+
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         eventMap = new HashMap<Marker, eventInfo>();
+        // Enable MyLocation Layer of Google Map
+        if(checkLocationPermission())
+            mMap.setMyLocationEnabled(true);
+
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        // set map type
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if(myLocation != null) {
+            // Get latitude of the current location
+            double latitude = myLocation.getLatitude();
+
+            // Get longitude of the current location
+            double longitude = myLocation.getLongitude();
+
+            // Create a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            // Show the current location in Google Map
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            // Zoom in the Google Map
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+        }
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
 
             @Override
@@ -99,6 +148,33 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnInfoWi
         System.out.println(point.latitude + "---" + point.longitude);
         eventMap.put(marker, data);
     }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public boolean checkLocationPermission()
+    {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+
 
 }
 
